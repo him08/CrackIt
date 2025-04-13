562f0bd4dbf488db4a83a93cfe31e506ceee8439
him08 <himanshivarshney25@gmail.com>
2025-04-13 11:52:47 +0530

    setup app,routing and auth pages

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,11 @@
+import { ReactNode} from 'react'
+import "../globals.css"
+
+const AuthLayout =({children}:{children: ReactNode}) =>{
+    return (
+        <div className="auth-layout">{children}</div>
+    )
+
+}
+export default AuthLayout;
+ 
